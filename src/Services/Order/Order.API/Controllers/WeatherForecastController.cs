cbd403ff4b7cde6c0c7ca8b4ccd6a908bff602a4
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2020-04-04 13:10:04 +0500

    Removes unnecessary files

--- src/Services/Order/Order.API/Controllers/WeatherForecastController.cs
+++ src/Services/Order/Order.API/Controllers/WeatherForecastController.cs
@@ -11,12 +11,7 @@ namespace Order.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
+        readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
@@ -24,16 +19,9 @@ namespace Order.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok();
         }
     }
 }
