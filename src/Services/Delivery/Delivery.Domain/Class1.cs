5fe50bd2951a51ce3ab47f89c003937af07d968a
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2020-04-17 20:29:53 +0500

    Removes Delivery service

--- src/Services/Delivery/Delivery.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Delivery.Domain
-{
-    public class Class1
-    {
-    }
-}
--- src/Services/Delivery/Delivery.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Delivery.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
