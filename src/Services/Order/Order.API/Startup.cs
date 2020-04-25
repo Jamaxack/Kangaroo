43135d54679c71713e7af251c7a3627968515ae4
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2020-04-25 19:09:27 +0500

    Adds Swagger to Order service

--- src/Services/Order/Order.API/Startup.cs
+++ src/Services/Order/Order.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Order.API.Infrastructure;
 using Order.API.Infrastructure.AutofacModules;
 using Order.Infrastructure;
@@ -26,19 +27,13 @@ namespace Order.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public virtual IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddEntityFrameworkSqlServer()
-                 .AddDbContext<DeliveryOrderContext>(options =>
-                 {
-                     options.UseSqlServer(Configuration["ConnectionString"],
-                         sqlServerOptionsAction: sqlOptions =>
-                         {
-                             sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
-                             sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
-                         });
-                 },
-                     ServiceLifetime.Scoped  //Showing explicitly that the DbContext is shared across the HTTP request scope (graph of objects started in the HTTP request)
-                 );
+            services
+                .AddControllers()
+                .AddNewtonsoftJson()
+                .Services
+                .AddCorsPolicy()
+                .AddSwagger()
+                .AddEntityFrameworkSqlServer(Configuration);
 
             //configure autofac
             var container = new ContainerBuilder();
@@ -50,7 +45,7 @@ namespace Order.API
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        { 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -59,17 +54,66 @@ namespace Order.API
                 context.Database.EnsureCreated();
                 new DeliveryOrderContextSeed().SeedAsync(context, env).Wait();
             }
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
+            app.UseCors("CorsPolicy");
+            app.UseSwagger()
+            .UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/v1/swagger.json", "Order.API V1");
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+    }
+
+    static class ExtensionsMethods
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Kangaroo - Order HTTP API",
+                    Version = "v1",
+                    Description = "The Order Service HTTP API"
+                });
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy => policy
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddEntityFrameworkSqlServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<DeliveryOrderContext>(options =>
+                {
+                    options.UseSqlServer(configuration["ConnectionString"],
+                        sqlServerOptionsAction: sqlOptions =>
+                        {
+                            sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                        });
+                },
+                    ServiceLifetime.Scoped  //Showing explicitly that the DbContext is shared across the HTTP request scope (graph of objects started in the HTTP request)
+                );
+            return services;
         }
     }
 }
