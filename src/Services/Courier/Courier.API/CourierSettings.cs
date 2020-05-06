35b828ed8b16077c67504e5534af763637e10db7
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2020-05-06 23:28:45 +0500

    Adds MongoDB context for Courier service

---
+++ src/Services/Courier/Courier.API/CourierSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Courier.API
+{
+    public class CourierSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
---
+++ src/Services/Courier/Courier.API/Infrastructure/CourierContext.cs
@@ -0,0 +1,44 @@
+﻿using Courier.API.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Courier.API.Infrastructure
+{
+    public class CourierContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public CourierContext(IOptions<CourierSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Model.Courier> Couriers
+        {
+            get
+            {
+                return _database.GetCollection<Model.Courier>("Couriers");
+            }
+        }
+
+        public IMongoCollection<CourierLocation> CourierLocations
+        {
+            get
+            {
+                return _database.GetCollection<CourierLocation>("CourierLocations");
+            }
+        }
+
+        public IMongoCollection<Delivery> Deliveries
+        {
+            get
+            {
+                return _database.GetCollection<Delivery>("Deliveries");
+            }
+        }
+
+
+    }
+}
