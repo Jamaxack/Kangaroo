ca8810be871063a8d0bced62bf4453cac8bd341f
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2020-05-17 23:15:48 +0500

    Removes ApiGateway project

--- src/ApiGateways/Web.Bff.Delivering/Web.Delivering.HttpAggregator/Config/UrlsConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Web.Delivering.HttpAggregator.Config
-{
-    public class UrlsConfig
-    {
-        public string Courier { get; set; }
-        public string GrpcCourier { get; set; }
-    }
-}
