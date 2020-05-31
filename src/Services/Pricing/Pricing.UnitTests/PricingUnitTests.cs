2f54c9c72a2fa870a8ff152a22c836e39c3c5720
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2020-05-31 19:11:43 +0500

    Adds new Pricing service

---
+++ src/Services/Pricing/Pricing.UnitTests/PricingUnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Pricing.UnitTests
+{
+    public class PricingUnitTests
+    {
+        [Fact]
+        public void AssertTrueShouldPassObviously()
+        {
+            Assert.True(true);
+        }
+    }
+}
