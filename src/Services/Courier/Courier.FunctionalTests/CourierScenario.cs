d55cb5fcedc38dac8593ed239e348de488315f79
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2020-06-14 16:28:54 +0500

    Adds functional test for Courier service Get Couriers Api

---
+++ src/Services/Courier/Courier.FunctionalTests/CourierScenario.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Courier.FunctionalTests
+{
+    public class CourierScenario : CourierScenarioBase
+    {
+        [Fact]
+        public async Task Couriers_get_should_response_ok_status_code()
+        {
+            using (var server = CreateTestServer())
+            {
+                var response = await server.CreateClient().GetAsync(Get.Couriers);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
---
+++ src/Services/Courier/Courier.FunctionalTests/CourierScenarioBase.cs
@@ -0,0 +1,32 @@
+﻿using Courier.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace Courier.FunctionalTests
+{
+    public class CourierScenarioBase
+    {
+        public TestServer CreateTestServer()
+        {
+            var path = Assembly.GetAssembly(typeof(CourierScenarioBase)).Location;
+
+            var hostBuilder = new WebHostBuilder()
+                .UseContentRoot(Path.GetDirectoryName(path))
+                .ConfigureAppConfiguration(contextBuilder =>
+                {
+                    contextBuilder.AddJsonFile("appsettings.json", optional: false).AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>();
+
+            return new TestServer(hostBuilder);
+        }
+
+        public static class Get
+        {
+            public static string Couriers = "api/v1/couriers"; 
+        } 
+    }
+}
